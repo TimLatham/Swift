e16689a1749fb63b0bbba11bc0e0d1de9623b8c6
Tim Latham <lathamtk@yahoo.com>
2017-07-28 10:55:10 -0400

    chore: Load completed playground 6

---
+++ IntroAppDevSwift/06_Functions.playground/Pages/Meme Exercise.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+/*:
+ ## Functional Meme
+ 
+ Think of a fun song or meme that you've heard or seen recently. Try to think of one with repetitive parts. For example:
+ 
+ - A song with a repeated chorus.
+ - A meme that repeats a word or phrase.
+ - A song that repeats a line over and over.
+ 
+ Write out the song or meme using `print` statements, then look for patterns and repetition and choose groups of lines to combine into functions. This is a free-form exercise, so you can do whatever you like.
+*/
+func bornToBeDown(){
+    print("Born to be down")
+}
+func firstVerse() {
+    bornToBeDown()
+    print("I've learned all my lessons before now")
+    bornToBeDown()
+    print("I think you'll get used to it")
+}
+
+func refrain() {
+    print()
+    print("And you just don't get it")
+    print("You keep it copacetic")
+    print("And you learn to accept it")
+    print("You know it's so pathetic")
+    print()
+}
+
+func secondVerse() {
+    bornToBeDown()
+    print("I think that I've said this before now")
+    bornToBeDown()
+    print("What good is confidence?")
+}
+
+firstVerse()
+refrain()
+secondVerse()
+refrain()
+//: Next, make the meme your own.
+//:
+//: [Previous](@previous)  |  page 11 of 12  |  [Next: Personal Meme](@next)
