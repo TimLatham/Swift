95d4085cb70b2ca968137ff80edae709c1d0f7ff
Tim Latham <lathamtk@yahoo.com>
2017-08-09 13:46:50 -0400

    chore: Load completed project 2.1

---
+++ AppDevSwift/Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+/*:
+ ## Exercise - String Basics
+ 
+ Create a `name` constant and assign it a string literal representing your name.
+ */
+let name = "Fritz Alfredo"
+
+/*:
+ Create a `favoriteQuote` constant and assign it the following string literal:
+ 
+ - "My favorite quote is <INSERT QUOTE HERE>."
+ 
+ Write in your own favorite quote where indicated, and be sure to include escaped quotation marks. Print the value of `favoriteQuote`
+ */
+let favoriteQuote = "My favorite quote is Aut inveniam viam aut faciam."
+
+/*:
+ Write an if-else statement that prints "There's nothing here" if `emptyString` is empty, and "It's not as empty as I thought" otherwise.
+ */
+let emptyString = ""
+print(emptyString == "" ? "There's nothing here":"It's not as empty as I though")
+
+//: page 1 of 5  |  [Next: Exercise - Concatenation and Interpolation](@next)
