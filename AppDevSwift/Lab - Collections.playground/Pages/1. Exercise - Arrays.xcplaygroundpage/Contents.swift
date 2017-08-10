2acd5d6a48f898b6944867d5841909c8767d2933
Tim Latham <lathamtk@yahoo.com>
2017-08-10 12:26:42 -0400

    chore: Load completed lab 2.5

---
+++ AppDevSwift/Lab - Collections.playground/Pages/1. Exercise - Arrays.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+/*:
+ ## Exercise - Arrays
+ 
+ Assume you are an event coordinator for a community charity event and are keeping a list of who has registered. Create a variable `registrationList` that will hold strings. It should be empty after initialization.
+ */
+var registrationList: [String] = []
+registrationList.append("Tim")
+registrationList.append("T")
+print(registrationList)
+registrationList.contains("T")
+registrationList.contains("Tim")
+
+print(registrationList.popLast()!)
+print(registrationList)
+registrationList.popLast()
+/*:
+ Your friend Sara is the first to register for the event. Add her name to `registrationList` using the `append` method. Print the contents of the collection.
+ */
+registrationList.append("Sarah")
+print(registrationList)
+/*:
+ Add four additional names into the array using the `+=` operator. All of the names should be added in one step. Print the contents of the collection.
+ */
+registrationList += ["Tim", "T", "Kyle", "Skyler"]
+print(registrationList)
+
+/*:
+ Use the `insert(_:at:)` method to add `Charlie` into the array as the second element. Print the contents of the collection.
+ */
+registrationList.insert("Charlie", at: 1)
+print(registrationList)
+/*:
+ Someone had a conflict and decided to transfer her registration to someone else. Use array subscripting to change the sixth element to `Rebecca`. Print the contents of the collection.
+ */
+registrationList[5] = "Rebecca"
+print(registrationList)
+/*:
+ Call `removeLast()` on `myArray`. If done correctly, this should remove `Rebecca` from the collection. Store the result of `removeLast()` into a new constant `deletedItem`, then print `deletedItem`.
+ */
+var deletedItem = registrationList.removeLast()
+print(deletedItem)
+//: page 1 of 4  |  [Next: App Exercise - Activity Challenge](@next)
